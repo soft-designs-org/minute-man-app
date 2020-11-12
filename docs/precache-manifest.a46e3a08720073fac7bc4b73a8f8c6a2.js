71ae90453ff5edb18beb0317d72a877066115222
soft-designs-org <dwintz@softdesigns.org>
2020-11-11 23:52:37 -0400

    initial commit

---
+++ docs/precache-manifest.a46e3a08720073fac7bc4b73a8f8c6a2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362643005a1e43112a97b8febfb6e0ed",
+    "url": "/minute-man-app/index.html"
+  },
+  {
+    "revision": "4158e499a8744f28c0a2",
+    "url": "/minute-man-app/static/css/main.3540fa18.chunk.css"
+  },
+  {
+    "revision": "9c210b13d1e6d3ba8756",
+    "url": "/minute-man-app/static/js/2.d002596e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/minute-man-app/static/js/2.d002596e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4158e499a8744f28c0a2",
+    "url": "/minute-man-app/static/js/main.ab90adcd.chunk.js"
+  },
+  {
+    "revision": "d943b2e4aac83ba00013",
+    "url": "/minute-man-app/static/js/runtime-main.10024414.js"
+  },
+  {
+    "revision": "d2d62a71f00a5f320a05e1194cacb2d9",
+    "url": "/minute-man-app/static/media/dp-img.d2d62a71.png"
+  },
+  {
+    "revision": "577a15443ba20865d5d500b2cd1980e5",
+    "url": "/minute-man-app/static/media/facebook-icon.577a1544.png"
+  },
+  {
+    "revision": "4168fcd96b9d756fe4292de97b0621d1",
+    "url": "/minute-man-app/static/media/instagram-icon.4168fcd9.png"
+  },
+  {
+    "revision": "28030290885bf5739bae2fc350f42da8",
+    "url": "/minute-man-app/static/media/slide1.28030290.jpg"
+  },
+  {
+    "revision": "5a90bf6d79ae6403e49073c6bc55432b",
+    "url": "/minute-man-app/static/media/slide2.5a90bf6d.jpg"
+  },
+  {
+    "revision": "42a2b69762d71b58c50d78af0039aea5",
+    "url": "/minute-man-app/static/media/slide3.42a2b697.jpg"
+  },
+  {
+    "revision": "ee81209e1d59093c9d8e063df5c40248",
+    "url": "/minute-man-app/static/media/twitter-icon.ee81209e.png"
+  },
+  {
+    "revision": "938db03e197bbc25e90d9652d4e38b4c",
+    "url": "/minute-man-app/static/media/user-avatar.938db03e.png"
+  }
+]);
